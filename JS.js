c87b604e91eb8e79da34e1d7f5bf8a9945ccc352
Mmitraa <117802325+Mmitraa@users.noreply.github.com>
2022-11-28 21:32:38 +0330

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,11 @@
+function minterm (){
+    document.getElementById("quis").innerHTML="exselen for you";
+}
+
+function sum (){
+    document.write (17593+529713);
+}
+function time(){
+    var date= Date();
+    document.write (date);
+}
